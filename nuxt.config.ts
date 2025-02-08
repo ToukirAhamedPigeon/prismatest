161cf505bf87fb0c30f7a16d1c741411da45b331
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-02-08 19:52:09 +0600

    All

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+      scanPageMeta: 'after-resolve',
+      sharedPrerenderData: false,
+      componentIslands: true,
+      compileTemplate: true,
+      resetAsyncDataToUndefined: true,
+      templateUtils: true,
+      relativeWatchPaths: true,
+      normalizeComponentNames: false,
+      spaLoadingTemplateLocation: 'within',
+      defaults: {
+        useAsyncData: {
+          deep: true
+        }
+      }
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
   devtools: { enabled: true }
 })
