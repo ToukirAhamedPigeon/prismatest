e325e5fab81198b56e1194396bdd796b1fde4f6e
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-02-10 23:20:39 +0600

    Layouts

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-3xl">Default Layout</h1>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
